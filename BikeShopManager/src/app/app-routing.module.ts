6ba8d791a0fec93b147a6774e8a862bcb0511892
tanqueta88 <oliverprim783@gmail.com>
2024-06-22 04:16:36 -0300

    Modifications in app-routing module

--- BikeShopManager/src/app/app-routing.module.ts
+++ BikeShopManager/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',//loclahost:4200/home
+    component: HomeComponent
+  },
+  {
+    path: 'admin',//loclahost:4200/admin
+    component: AdminComponent
+  },
+  {
+    path: 'galeria',//loclahost:4200/galeria
+    component: AdminComponent
+  },
+  {
+    path: 'image/id',//loclahost:4200/image/${idImagenQueSeHaceClick}
+    component: AdminComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
