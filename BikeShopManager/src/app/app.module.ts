95ae0842a959f4c4668c9daa4d5523bafc0f17c8
tanqueta88 <oliverprim783@gmail.com>
2024-06-21 23:55:13 -0300

    ReactiveFormModule is added

--- BikeShopManager/src/app/app.module.ts
+++ BikeShopManager/src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule, //Se agrega el modulo para el manejo de forularios reactivos
     AppRoutingModule
   ],
   providers: [],
